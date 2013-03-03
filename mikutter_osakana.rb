2288cb34aa609a0cfe5b9ec97e57e394f59e8fcf
Taiki ONO <taiks.4559@gmail.com>
2013-03-03 22:17:56 +0900

    add posting with osakana

--- mikutter_osakana.rb
+++ mikutter_osakana.rb
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
+require 'pry'
 
 Plugin.create :mikutter_osakana do
-  STDOUT.puts 'aaa'
+  aa = open(File.expand_path('../aa.text', __FILE__)).read.chomp
+
+  command(
+    :osakana_tweet,
+    name: 'おさかなといっしょにつぶやく',
+    condition: Plugin::Command[:Editable],
+    visible: false,
+    role: :postbox
+  ) do |opt|
+    buff = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer
+    buff.text = aa + buff.text
+    opt.widget.post_it!
+  end
 end
