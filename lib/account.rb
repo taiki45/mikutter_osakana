997202c30ae794eb654d05739b2dfc2336077a08
Taiki ONO <taiks.4559@gmail.com>
2013-03-04 21:09:24 +0900

    split files

---
+++ lib/account.rb
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+module Osakana
+  class Account
+    attr_reader :level
+
+    def initialize
+      @level = load_status
+    end
+
+    def load_status
+      exp = open(save_file).read.chomp.to_i
+      Level.from(exp)
+    end
+
+    def save
+      open(save_file, 'w') {|f| f.write @level.exp }
+    end
+
+    def save_file
+      dst = File.expand_path('../../exp', __FILE__)
+      open(dst, 'w') {|f| f.write '0' } unless File.exist? dst
+      dst
+    end
+
+    def increase(event)
+      __send__('on_' + event.to_s)
+    end
+
+    def on_post
+      @level.add rand(100..300)
+    end
+
+    def on_fav
+      @level.add rand(10..50)
+    end
+
+    def on_faved
+      @level.add 10
+    end
+
+    def on_apper
+      @level.add 1
+    end
+  end
+end
---
+++ lib/level.rb
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+module Osakana
+  class Level
+    def self.from(exp)
+      level = ExpTable.to_enum.each_with_index do |e, i|
+        break i if e > exp
+      end
+      self.new level, exp
+    end
+
+    attr_reader :level, :exp
+
+    def initialize(level, exp = nil)
+      @level = level
+      @exp = exp || ExpTable.thresholds_at(level)
+    end
+
+    def add(exp)
+      @exp += exp
+    end
+
+    def force
+      self.class.from @exp
+    end
+
+    def to_i
+      force.level.to_i
+    end
+
+    def to_s
+      force.level.to_s
+    end
+
+    def method_missing(name, *args)
+      force.level.__send__ name, *args
+    end
+
+    def respond_to_missing(name)
+      force.level.respond_to_missing name
+    end
+
+    module ExpTable
+      extend Enumerable
+
+      class << self
+        def cache
+          @cache ||= []
+        end
+
+        def [](index)
+          cache[index] || cache[index] = threshold_at(index)
+        end
+
+        def threshold_at(n)
+          (1..n).map(&method(:rate)).reduce(&:+)
+        end
+
+        def rate(n)
+          n * Math.log(n * 100).to_i
+        end
+
+        def to_enum
+          Enumerator.new do |y|
+            i = 1
+            loop do
+              y << self[i]
+              i += 1
+            end
+          end
+        end
+      end
+    end
+  end
+end
